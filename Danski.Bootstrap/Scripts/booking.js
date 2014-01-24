53e6ed2539f5f6154318420e4ae95b2529f7064c
Smitche <martinsmidtk@hotmail.com>
2014-01-24 16:19:08 +0100

    Merged med Ophold branch

---
+++ Danski.Bootstrap/Scripts/booking.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    /*----------------------------------------------------*/
+    /*	Booking Accordion
+    /*----------------------------------------------------*/
+
+    $('.panel-title').click(function () {
+        var state = $(this).find('a').attr('class');
+        var arrow = $(this).find('span');
+
+        if (state == "") {
+            arrow.addClass('icon-arrow-down6');
+            arrow.removeClass('icon-arrow-up5');
+        }
+        if (state == "collapsed") {
+            arrow.addClass('icon-arrow-up5');
+            arrow.removeClass('icon-arrow-down6');
+        }
+    })
+
+});
