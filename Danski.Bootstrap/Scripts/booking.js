ffa2b2decaaa7a0cc343c7c0427e2132a59c5012
Smitche <martinsmidtk@hotmail.com>
2014-01-29 17:10:19 +0100

    style ændringer + checkbox js

--- Danski.Bootstrap/Scripts/booking.js
+++ Danski.Bootstrap/Scripts/booking.js
@@ -3,6 +3,7 @@
     /*	Booking Accordion
     /*----------------------------------------------------*/
 
+    // change arrow up and down
     $('.panel-title').click(function () {
         var state = $(this).find('a').attr('class');
         var arrow = $(this).find('span');
@@ -16,5 +17,16 @@
             arrow.removeClass('icon-arrow-down6');
         }
     })
+
+    // show/hide traveller-choices content
+    var checked = $('#checkboxChoicesNow').is(':checked')  // Boolean true
+    
+    $('#checkboxChoicesNow').click(function () {
+        if (checked) {
+            $('.traveller-choices-wrapper').slideUp();
+            alert(checked);
+        }
+    })
+
 
 });
