e3cbef314c149feeaa3a86be387caf26d15fe539
vonlinaa <soren@beon.dk>
2014-02-06 14:31:07 +0100

    ændringer

--- Danski.Bootstrap/Scripts/Custom.js
+++ Danski.Bootstrap/Scripts/Custom.js
@@ -139,6 +139,24 @@
      });
  })(jQuery);
 
+////////////////////
+// Fix menu to close
+///////////////////
+ $(document).on('click', '.yamm .dropdown-menu', function (e) {
+     e.stopPropagation();
+ })
+
+////////////////////
+// Windows 8 phone fix
+///////////////////
+ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+     var msViewportStyle = document.createElement('style');
+         msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+         document.querySelector('head').appendChild(msViewportStyle);
+ }
+
+
+
 /* --------------------------------------------------------------------------- */
 /*  Google Maps
 /* --------------------------------------------------------------------------- */
